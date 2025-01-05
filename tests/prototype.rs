681d1e14d89a4478c9b39327dc70beed4cebdbd8
Daniel Müller <deso@posteo.net>
2025-01-05 13:23:15 -0800

    Test

--- tests/prototype.rs
+++ tests/prototype.rs
@@ -9,5 +9,15 @@
 
 #[test_log::test]
 fn it_works() {
+  tracing::debug!("tracing::DEBUG");
+  tracing::info!("tracing::INFO");
+  tracing::warn!("tracing::WARN");
+  tracing::error!("tracing::ERROR");
+
+  logging::debug!("log::DEBUG");
+  logging::info!("log::INFO");
+  logging::warn!("log::WARN");
+  logging::error!("log::ERROR");
+
   assert_eq!(2 + 2, 4);
 }
